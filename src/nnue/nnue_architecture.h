e24566eac88f820747cff4c3a23a739953d4f8c6
JWmer <julian.willemer@online.de>
2021-12-13 11:01:37 +0000

    Update nnue_architecture.h

--- src/nnue/nnue_architecture.h
+++ src/nnue/nnue_architecture.h
@@ -35,7 +35,7 @@ namespace Stockfish::Eval::NNUE {
   using FeatureSet = Features::HalfKAv2_hm;
 
   // Number of input feature dimensions after conversion
-  constexpr IndexType TransformedFeatureDimensions = 1024;
+  constexpr IndexType TransformedFeatureDimensions = 32;
   constexpr IndexType PSQTBuckets = 8;
   constexpr IndexType LayerStacks = 8;
 
@@ -44,7 +44,7 @@ namespace Stockfish::Eval::NNUE {
     // Define network structure
     using InputLayer = InputSlice<TransformedFeatureDimensions * 2>;
     using HiddenLayer1 = ClippedReLU<AffineTransform<InputLayer, 8>>;
-    using HiddenLayer2 = ClippedReLU<AffineTransform<HiddenLayer1, 32>>;
+    using HiddenLayer2 = ClippedReLU<AffineTransform<HiddenLayer1, 8>>;
     using OutputLayer = AffineTransform<HiddenLayer2, 1>;
 
   }  // namespace Layers
